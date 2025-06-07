0e41e6bf030d79f35791c8014ed4fd7838c04977
herloncosta <herlon36@gmail.com>
2025-06-06 21:54:58 -0300

    update

--- challenges/somar-multiplos-arrays.js
+++ challenges/somar-multiplos-arrays.js
@@ -2,22 +2,20 @@ const array1 = [47, 2, 58, 3, 6];
 const array2 = [9, 82, 17, 36, 6, 2];
 const array3 = [14, 36, 5, 8];
 
-// encontrar o array com o maior tamanho
-function retornarArrayComMaiorTamanho(...arrays) {
-  let maior = arrays[0].length;
+function retornarMaiorTamanhoDeUmArray(...arrays) {
+  let maiorTamanho = arrays[0].length;
   for (let i = 1; i < arrays.length; i++) {
-    if (arrays[i].length > maior) maior = arrays[i].length;
+    if (arrays[i].length > maiorTamanho) maiorTamanho = arrays[i].length;
   }
-  return maior;
+  return maiorTamanho;
 }
 
-const arrayMaiorTamanho = retornarArrayComMaiorTamanho(array1, array2, array3);
+const arrayMaiorTamanho = retornarMaiorTamanhoDeUmArray(array1, array2, array3);
 
 let total1 = 0;
 let total2 = 0;
 let total3 = 0;
 
-// somar os valores de cada array
 for (let i = 0; i < arrayMaiorTamanho; i++) {
   total1 += array1[i] || 0;
   total2 += array2[i] || 0;
