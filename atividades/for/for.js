dc2b98f69c75a7665a6bdbe1bc576e1b3878df68
herloncosta <herlon36@gmail.com>
2025-06-22 12:43:53 -0300

    learning for in Javascript

---
+++ atividades/for/for.js
@@ -0,0 +1,10 @@
+// for (inicialização; condição; expressão final) {
+// bloco de código
+// }
+
+//              [0, 1, 2, 3, 4]
+const numeros = [1, 2, 3, 4, 5];
+
+for (let indice = 0; indice < numeros.length; indice += 1) {
+  console.log(numeros[indice]);
+}
